6661666202cd2633fbb27fee8efbd3a3624e883e
Branden Ge <86636108+brandenge@users.noreply.github.com>
2022-09-20 00:28:54 -0500

    Modularized server ready for mock testing

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 'use strict';
 
-const express = require('express');
-const notFound = require('./error-handlers/404');
-const errorHandler = require('./error-handlers/500');
-const PORT = process.env.PORT || 3002;
+const { start } = require('./src/app');
 
-const app = express();
-
-app.get('/', (req, res, next) => {
-  res.status(200).send('Hello World from my Express server');
-});
-
-app.get('/bad', (req, res, next) => {
-  next('This is a bad route that resulted in an error');
-});
-
-app.use('*', notFound);
-
-app.use(errorHandler);
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+start();
